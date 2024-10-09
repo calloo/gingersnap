fc24f9f51844e6c8909b1db3f6fb2219112cd2a8
DrScript <dev@cookienerds.com>
2024-10-09 09:23:02 +0800

    remove currying

--- src/functools/index.ts
+++ src/functools/index.ts
@@ -7,20 +7,18 @@ import { FutureCancelled } from "../errors";
 /**
  * Returns true if all elements of the stream match the predicate, false if there are any that don't.
  */
-export const all = R.curry(
-  <T>(predicate: (v: T) => boolean | Future<boolean>, stream: Stream<T>): Future<boolean> =>
-    stream
-      .map(predicate)
-      .skipWhile(R.equals(true))
-      .take(1)
-      .collect(Collectors.counting())
-      .thenApply(({ value }) => value === 0)
-);
+export const all = <T>(predicate: (v: T) => boolean | Future<boolean>, stream: Stream<T>): Future<boolean> =>
+  stream
+    .map(predicate)
+    .skipWhile(R.equals(true))
+    .take(1)
+    .collect(Collectors.counting())
+    .thenApply(({ value }) => value === 0);
 
 /**
  * Returns a new stream containing the contents of the given stream, followed by the given element.
  */
-export const append = R.curry(<T>(value: T | Future<T>, stream: Stream<T>): Stream<T> => {
+export const append = <T>(value: T | Future<T>, stream: Stream<T>): Stream<T> => {
   return Stream.of(async function* (signal: AbortSignal) {
     stream.cancelOnSignal(signal);
 
@@ -34,12 +32,12 @@ export const append = R.curry(<T>(value: T | Future<T>, stream: Stream<T>): Stre
       yield value;
     }
   }) as Stream<T>;
-});
+};
 
 /**
  * Returns a new stream with the given element at the front, followed by the contents of the stream.
  */
-export const prepend = R.curry(<T>(value: T | Future<T>, stream: Stream<T>): Stream<T> => {
+export const prepend = <T>(value: T | Future<T>, stream: Stream<T>): Stream<T> => {
   return Stream.of(async function* (signal: AbortSignal) {
     stream.cancelOnSignal(signal);
     if (value instanceof Future) {
@@ -52,12 +50,12 @@ export const prepend = R.curry(<T>(value: T | Future<T>, stream: Stream<T>): Str
       yield data;
     }
   }) as Stream<T>;
-});
+};
 
 /**
  * Returns the result of concatenating the given streams.
  */
-export const concat = R.curry(<T>(value: Stream<T>, stream: Stream<T>): Stream<T> => {
+export const concat = <T>(value: Stream<T>, stream: Stream<T>): Stream<T> => {
   return Stream.of(async function* (signal: AbortSignal) {
     value.cancelOnSignal(signal);
     stream.cancelOnSignal(signal);
@@ -69,93 +67,84 @@ export const concat = R.curry(<T>(value: Stream<T>, stream: Stream<T>): Stream<T
       yield data;
     }
   }) as Stream<T>;
-});
+};
 
 export const head = <T>(stream: Stream<T>): Future<T> => stream.head().execute();
 
 export const tail = <T>(stream: Stream<T>): Future<T> => stream.tail().execute();
 
-export const filter = R.curry(
-  <T>(predicate: (v: T) => boolean | Future<boolean>, stream: Stream<T>): Stream<T> => stream.filter(predicate)
-);
+export const filter = <T>(predicate: (v: T) => boolean | Future<boolean>, stream: Stream<T>): Stream<T> =>
+  stream.filter(predicate);
 
-export const map = R.curry(<T, V>(predicate: (v: T) => V | Future<V>, stream: Stream<T>) => stream.map(predicate));
+export const map = <T, V>(predicate: (v: T) => V | Future<V>, stream: Stream<T>) => stream.map(predicate);
 
-export const insert = R.curry(
-  <T>(index: number, value: T | Future<T>, stream: Stream<T>) =>
-    Stream.of(async function* (signal: AbortSignal) {
-      stream.cancelOnSignal(signal);
-      let i = -1;
-      let inserted = false;
+export const insert = <T>(index: number, value: T | Future<T>, stream: Stream<T>) =>
+  Stream.of(async function* (signal: AbortSignal) {
+    stream.cancelOnSignal(signal);
+    let i = -1;
+    let inserted = false;
 
-      for await (const data of stream) {
-        if (!inserted) {
-          i++;
-        }
-
-        if (i === index) {
-          inserted = true;
-          if (value instanceof Future) {
-            yield await value;
-          } else {
-            yield value;
-          }
-        }
-        yield data;
+    for await (const data of stream) {
+      if (!inserted) {
+        i++;
       }
-    }) as Stream<T>
-);
 
-export const insertAll = R.curry(
-  <T>(index: number, value: T[] | Array<Future<T>> | Stream<T>, stream: Stream<T>) =>
-    Stream.of(async function* (signal: AbortSignal) {
-      stream.cancelOnSignal(signal);
-      let i = -1;
-      let inserted = false;
-
-      for await (const data of stream) {
-        if (!inserted) {
-          i++;
+      if (i === index) {
+        inserted = true;
+        if (value instanceof Future) {
+          yield await value;
+        } else {
+          yield value;
         }
+      }
+      yield data;
+    }
+  }) as Stream<T>;
 
-        if (i === index) {
-          inserted = true;
-          if (value instanceof Stream) {
-            value.cancelOnSignal(signal);
-            for await (const data of value) {
+export const insertAll = <T>(index: number, value: T[] | Array<Future<T>> | Stream<T>, stream: Stream<T>) =>
+  Stream.of(async function* (signal: AbortSignal) {
+    stream.cancelOnSignal(signal);
+    let i = -1;
+    let inserted = false;
+
+    for await (const data of stream) {
+      if (!inserted) {
+        i++;
+      }
+
+      if (i === index) {
+        inserted = true;
+        if (value instanceof Stream) {
+          value.cancelOnSignal(signal);
+          for await (const data of value) {
+            yield data;
+          }
+        } else {
+          for (const data of value) {
+            if (data instanceof Future) {
+              yield await data;
+            } else {
               yield data;
             }
-          } else {
-            for (const data of value) {
-              if (data instanceof Future) {
-                yield await data;
-              } else {
-                yield data;
-              }
-            }
           }
         }
-        yield data;
       }
-    }) as Stream<T>
-);
+      yield data;
+    }
+  }) as Stream<T>;
 
-export const slice = R.curry(<T>(start: number, end: number, stream: Stream<T>) =>
-  stream.skip(start).take(end - start)
-);
+export const slice = <T>(start: number, end: number, stream: Stream<T>) => stream.skip(start).take(end - start);
 
-export const repeat = R.curry(
-  <T>(value: Future<T>, count: number) =>
-    Stream.of(async function* (signal: AbortSignal) {
-      const result = await value.registerSignal(signal);
+export const repeat = <T>(value: Future<T>, count: number) =>
+  Stream.of(async function* (signal: AbortSignal) {
+    const result = await value.registerSignal(signal);
 
-      for (let i = 0; i < count; i++) {
-        yield result;
-      }
-    }) as Stream<T>
-);
+    for (let i = 0; i < count; i++) {
+      yield result;
+    }
+  }) as Stream<T>;
 
-export const join = R.curry((separator: string, stream: Stream<string>) => {
+export const join = (separator: string, stream: Stream<string>) => {
   let result = "";
   return stream
     .forEach((value) => {
@@ -167,9 +156,9 @@ export const join = R.curry((separator: string, stream: Stream<string>) => {
       }
       return result;
     });
-});
+};
 
-export const without = R.curry(<T>(value: T[] | Future<T[]>, stream: Stream<T>) =>
+export const without = <T>(value: T[] | Future<T[]>, stream: Stream<T>) =>
   value instanceof Future
     ? Stream.of(async function* (signal: AbortSignal) {
         const result = await value.registerSignal(signal);
@@ -178,30 +167,32 @@ export const without = R.curry(<T>(value: T[] | Future<T[]>, stream: Stream<T>) 
           yield v;
         }
       })
-    : stream.filter((v) => !value.includes(v))
-);
+    : stream.filter((v) => !value.includes(v));
 
-export const repeatUntilSuccess = R.curry(
-  <T>(retries: number, waitPeriodInSeconds: number, backoffFactor: number, functor: () => Future<T>) =>
-    Future.of<T>(async (_, __, signal) => {
-      let lastError: unknown;
-      let i = 0;
-      let waitPeriod = waitPeriodInSeconds;
+export const repeatUntilSuccess = <T>(
+  retries: number,
+  waitPeriodInSeconds: number,
+  backoffFactor: number,
+  functor: () => Future<T>
+) =>
+  Future.of<T>(async (_, __, signal) => {
+    let lastError: unknown;
+    let i = 0;
+    let waitPeriod = waitPeriodInSeconds;
 
-      while (i < retries && !signal.aborted) {
-        try {
-          return await functor().registerSignal(signal);
-        } catch (e: unknown) {
-          lastError = e;
-          i++;
-          await Future.sleep({ seconds: waitPeriod });
-          waitPeriod *= backoffFactor;
-        }
+    while (i < retries && !signal.aborted) {
+      try {
+        return await functor().registerSignal(signal);
+      } catch (e: unknown) {
+        lastError = e;
+        i++;
+        await Future.sleep({ seconds: waitPeriod });
+        waitPeriod *= backoffFactor;
       }
+    }
 
-      throw lastError || new FutureCancelled();
-    })
-);
+    throw lastError || new FutureCancelled();
+  });
 
 export const range = (start: number, stop: number | undefined = undefined, step = 1) =>
   Stream.of(async function* (signal: AbortSignal) {
@@ -214,4 +205,4 @@ export const range = (start: number, stop: number | undefined = undefined, step 
     }
   });
 
-export const nth = R.curry(<T>(index: number, stream: Stream<T>) => stream.skip(index).future);
+export const nth = <T>(index: number, stream: Stream<T>) => stream.skip(index).future;
